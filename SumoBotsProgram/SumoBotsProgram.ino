c40c204030a5f181922b287e59fdde6f842722a8
Gurman <g9brar@edu.uwaterloo.ca>
2019-11-19 11:14:14 -0500

    Update SumoBotsProgram.ino

--- SumoBotsProgram/SumoBotsProgram.ino
+++ SumoBotsProgram/SumoBotsProgram.ino
@@ -12,19 +12,87 @@
 * Create a function for right turn
 * Create a function for attacking
 * Create function for defense
-* Create function for ultrasonic sensor (F,R,L)
-* Create function for color sensor
+* Create function for ultrasonic sensor (F,R,L)  
 * Create function for PIR sensor 
 */
 
+#define s0 4
+#define s1 5
+#define s2 6
+#define s3 7
+#define outPin 8
+
+int red;
+int blue;
+int grn;
+String color = "";
+
+float avgRed = 0;
+float avgGreen = 0;
+float avgBlue = 0;
 
 
-void setup() {
-  // put your setup code here, to run once:
+void whiteAvoidance()
+{
+  readRGB();
+
+  if(red > 8 && red < 18 && grn > 9 && grn < 19 && blue > 8 && blue < 16)
+  {
+    //TO DO:
+    //CODE FOR COLOR AVOIDANCE TO STAY IN THE RING
+  }
+}
+
+//Function that reads the RGB components of the color by taking an average to reduce error
+void readRGB()
+{
+  red = 0;
+  grn = 0;
+  blue = 0;
+  int iter = 10;
+  
+  //Loop the RGB check 3 times to find an average and avoid any partial errors
+  for(int i = 0; i < iter; i++)
+  {
+  
+  // By setting s2 and s3 to low we are reading for red components of light.
+  digitalWrite(s2, LOW);
+  digitalWrite(s3, LOW);
+  red = red + pulseIn(outPin, LOW);
+
+  // By setting s2 and s3 to low we are reading for green components of light.
+  digitalWrite(s2, HIGH);
+  digitalWrite(s3, HIGH);
+  grn = grn + pulseIn(outPin, LOW);
+
+  // By setting s2 and s3 to low we are reading for blue components of light.
+  digitalWrite(s2, LOW);
+  digitalWrite(s3, HIGH);
+  blue = blue + pulseIn(outPin, LOW);
+}
+  //Taking the average
+  red = red / iter;
+  grn = grn / iter;
+  blue = blue / iter;
+}
+
+
+void setup() 
+{
+  pinMode(s0, OUTPUT);
+  pinMode(s1, OUTPUT);
+  pinMode(s2, OUTPUT);
+  pinMode(s3, OUTPUT);
+  pinMode(outPin, INPUT);
+  
+  //Initializing the frequency scaling to 100% for more accuracte color detection.
+  digitalWrite(s0,HIGH);
+  digitalWrite(s1, HIGH);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop()
+{
+  whiteAvoidance();
 }
