72208642fb7c99eec48fd4742f68f20cdb473a78
Gurman <g9brar@edu.uwaterloo.ca>
2019-11-19 10:33:03 -0500

    Update SumoBotsProgram.ino

--- SumoBotsProgram/SumoBotsProgram.ino
+++ SumoBotsProgram/SumoBotsProgram.ino
@@ -1,3 +1,24 @@
+/* AUTHORS: Gurman Brar, Eknoor Singh, Jagroop Gill
+ * DATE: NOV.19 - NOV.24
+ * PURPOSE: Create an autonomous sumo bot that is able to defend,
+ * attack and move on its own, responding to different 
+ * sensory feedback from its enviroment.
+ */
+
+
+
+/*TO DO:
+* Create Function for left turn
+* Create a function for right turn
+* Create a function for attacking
+* Create function for defense
+* Create function for ultrasonic sensor (F,R,L)
+* Create function for color sensor
+* Create function for PIR sensor 
+*/
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
